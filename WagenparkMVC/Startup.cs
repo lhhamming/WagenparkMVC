6e7c949070d6d32fdb1888afc73bdd036a685390
Luuk Hamming <luuk.hamming@gmail.com>
2018-06-06 11:50:07 +0200

    Add project files.

---
+++ WagenparkMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WagenparkMVC.Startup))]
+namespace WagenparkMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
