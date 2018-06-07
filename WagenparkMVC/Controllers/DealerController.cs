b3bcefbc27bcc28ad9d12ede3611406c1745e03b
Luuk Hamming <luuk.hamming@gmail.com>
2018-06-07 13:35:07 +0200

    Added a way to access the dealer page

---
+++ WagenparkMVC/Controllers/DealerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WagenparkMVC.Controllers
+{
+    public class DealerController : Controller
+    {
+        // GET: Dealer
+        public ActionResult DealerPage()
+        {
+            return View();
+        }
+    }
+}
